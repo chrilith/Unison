a949bb22cc08f1369f97459c39f13d374f5074c6
Chris Apers <chrilith@hotmail.com>
2015-10-20 22:17:54 +0200

    More options for the autoloader

--- src/Initialization/AutoLoader.php
+++ src/Initialization/AutoLoader.php
@@ -14,9 +14,11 @@ class AutoLoader {
 	}
 
 	private $paths;
+	private $prefixes;
 
 	private function __construct() {
 		$this->paths = array('__any' => array());
+		$this->prefixes = array();
 	}
 
 	public function register() {
@@ -27,20 +29,68 @@ class AutoLoader {
 		}
 	}
 
+	private function append(&$array, $key, $value) {
+		if (!array_key_exists($key, $array)) {
+			$array[$key] = array();
+		}
+
+		if (substr($value, -1) != DIRECTORY_SEPARATOR) {
+			$value .= DIRECTORY_SEPARATOR;
+		}
+
+		array_push($array[$key], $value);
+	}
+
+	/**
+	 * Sets the default search path if no ns() or prefix() keys match the requested namespace.
+	 *
+	 * @param  string $path
+	 */
 	public function path($path) {
-		// TODO: prevent duplicates with missing ending /
-		array_push($this->paths['__any'], $path);
+		$this->ns('__any', $path);
 	}
 
 	public function ns($ns, $path) {
-		if (!array_key_exists($ns, $this->paths)) {
-			$this->paths[$ns] = array();
-		}
-
-		array_push($this->paths[$ns], $path);	
+		$this->append($this->paths, $ns, $path);
 	}
 
-	private function searchFile($ns, $file) {
+	public function prefix($prefix, $path) {
+		$this->append($this->prefixes, $prefix, $path);
+	}
+
+	/**
+	 * Searchs for a path replacement matching a namespace prefix iterating on the namespace elements.
+	 *
+	 * @param  string $ns   Namespace to resolve.
+	 * @param  string $file Filename to find.
+	 *
+	 * @return string|boolean
+	 *         Path to the class file or FALSE if not found.
+	 */
+	private function searchPrefix($ns, $file) {
+		$pos = $len = strlen($ns);
+
+		do {
+			$key = substr($ns, 0, $pos);
+			$part = str_replace('\\', DIRECTORY_SEPARATOR, substr($ns, $pos + 1)) . DIRECTORY_SEPARATOR;
+
+			if (array_key_exists($key, $this->prefixes)) {
+				$paths = $this->prefixes[$key];
+
+				foreach ($paths as $path) {
+					$fileName = $path . $part . $file;
+					if (is_readable($fileName)) {
+						return $fileName;
+					}
+				}
+			}
+
+		} while (($pos = strrpos($ns, '\\', $pos - $len - 1)) !== FALSE);
+
+		return FALSE;
+	}
+
+	private function searchNamespace($ns, $file) {
 		if (!array_key_exists($ns, $this->paths)) {
 			$ns = '__any';
 		}
@@ -48,13 +98,13 @@ class AutoLoader {
 		$paths = $this->paths[$ns];
 
 		foreach ($paths as $path) {
-			// Search Path+NS+File
+			// Search Path + (NS Path + File)
 			$fileName = $path . $file;
 			if (is_readable($fileName)) {
 				return $fileName;
 			}
 
-			// Search Path+File
+			// Search Path + (File)
 			if (($last = strrpos($file, DIRECTORY_SEPARATOR)) !== FALSE) {
 				$fileName = $path . substr($file, $last + 1);
 				if (is_readable($fileName)) {
@@ -63,7 +113,7 @@ class AutoLoader {
 			}
 		}
 
-		return $file;
+		return FALSE;
 	}
 
 	private function autoload($className) {
@@ -72,14 +122,23 @@ class AutoLoader {
 		$fileName  = '';
 		$namespace = '';
 
+		// For Foo\Bar\MyClass
 		if (($lastPos = strrpos($className, '\\')) !== FALSE) {
-	        $namespace = substr($className, 0, $lastPos);
-	        $className = substr($className, $lastPos + 1);
-	        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	        $namespace = substr($className, 0, $lastPos);	// => Foo\Bar
+	        $className = substr($className, $lastPos + 1);	// => MyClass
+	        $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;	// => Foo/Bar/
 	    }
-	    $fileName = $this->searchFile($namespace, $fileName . $className . '.php');
 
-    	require $fileName;
+	    // ("Foo\Bar", "Foo/Bar/MyClass.php")
+	    $fileName = $this->searchNamespace($namespace, $fileName . $className . '.php');
+	    if ($fileName === FALSE) {
+	    	$fileName = $this->searchPrefix($namespace, $className . '.php');
+	    }
+	    if ($fileName === FALSE) {
+	    	throw new \Exception('Cannot find class "' . $namespace . '\\' . $className . '".');
+	    }
+	    // Try to load the class if any
+	    require $fileName;
 	}
 
 }
